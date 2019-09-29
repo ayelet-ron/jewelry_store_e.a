cbed9dc97bb2643b5069a94c80d50e667f7bf1fa
ayelet-ron <ayeletron161@gmail.com>
2019-09-29 15:04:46 +0300

    Add project files.

---
+++ Jewelry_Store_e.a/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Jewelry_Store_e.a.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
